8bf49b38f4d1ef493ee530c92952c9a41bc458ac
MdFahad23 <fahadbdctglh8@gmail.com>
2023-08-27 19:17:07 +0600

    Create getUser for Id

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,4 +1,6 @@
 const createError = require("http-errors");
+const mongoose = require("mongoose")
+
 const { User } = require("../models/userModel");
 const { successResponse } = require("../helper/responseHandler");
 
@@ -9,7 +11,7 @@ module.exports.getUsers = async (req, res, next) => {
 
     const search = req.query.search || "";
     const page = Number(req.query.page) || 1;
-    const limit = Number(req.query.limit) || 1;
+    const limit = Number(req.query.limit) || 5;
 
     const serachRegExp = new RegExp('.*' + search + '.*', 'i')
 
@@ -45,3 +47,28 @@ module.exports.getUsers = async (req, res, next) => {
     next(error);
   }
 };
+
+// Get Single User 
+module.exports.getUser = async (req, res, next) => {
+  try {
+    const id = req.params.id
+    const options = { password: 0 }
+
+    const user = await User.findById(id, options)
+
+    if (!user) throw createError(404, 'user does not exist with this id')
+
+    return successResponse(res, {
+      statusCode: 200, message: "user were returned Successfully!", payload: {
+        user
+      }
+    })
+
+  } catch (error) {
+    if (error instanceof mongoose.Error) {
+      next(createError(400, 'Invalid User Id'))
+      return
+    }
+  }
+}
+
--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,8 +1,10 @@
 const router = require("express").Router();
 
-const { getUsers } = require("../controllers/userController");
+const { getUsers, getUser } = require("../controllers/userController");
 
 // GET: api/users
 router.route("/").get(getUsers);
+// Get: api/users/:id
+router.route("/:id").get(getUser)
 
 module.exports = router;
